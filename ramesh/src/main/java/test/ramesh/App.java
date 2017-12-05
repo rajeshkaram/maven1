080c9bedf3035d963363031d3f36eecd70faf765
RAMAGIRI <RAMAGIRI@192.168.0.6>
2017-12-05 20:42:49 +0530

    addin code

---
+++ ramesh/src/main/java/test/ramesh/App.java
@@ -0,0 +1,13 @@
+package test.ramesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
